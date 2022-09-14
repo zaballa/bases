48c2459df0cc9d6cabd0404091a62c8c5296b918
zaballa <zaballalandajo@hotmail.com>
2022-09-14 18:45:06 +0200

    prueba

--- create database militar;.sql
+++ create database militar;.sql
@@ -4,5 +4,7 @@ create table Servicio(codigo_servicio int, descripcion varchar(10),PRIMARY KEY(c
 create table Compañia(codigo_compañia int, actividad principal varchar(50),PRIMARY KEY(codigo_compañia));
 create table Cuartel(codigo_cuartel int, nombre varchar(10),PRIMARY KEY(codigo_cuartel));
 create table Soldado(codigo_soldado int, nombre varchar(10), apellido1 varchar(20) ,apellido2 varchar(20) ,codigo_cuartel int ,codigo_compañia int,codigo_cuerpo int, PRIMARY KEY(codigo_soldado, FOREIGN KEY(codigo_cuartel) REFERENCES Cuartel(codigo_cuartel),FOREIGN KEY(codigo_compañia) REFERENCES Compañia(codigo_compañia),FOREIGN KEY(codigo_cuerpo) REFERENCES Cuerpo(codigo_cuerpo));
+
 create table Sol_servicio(fecha datetime,codigo_soldado int,codigo_servicio int, PRIMARY KEY(codigo_soldado),PRIMARY KEY(codigo_servicio));
 create table Sol_graduacion(fecha_inicio datetime,codigo_soldado int,PRIMARY KEY(codigo_soldado,fecha_inicio));
+
